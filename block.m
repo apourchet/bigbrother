2368b38fc6278c789bb9930d046334c2a4c44dd9
Antoine Pourchet <antoine.pourchet@gmail.com>
2015-10-28 18:02:03 -0400

    Added stubs for block and drone

---
+++ block.m
@@ -0,0 +1,10 @@
+classdef Block
+    properties
+        last_observed
+    end
+    methods
+        function obj = Block()
+            % Constructor
+        end
+    end
+end
---
+++ drone.m
@@ -0,0 +1,10 @@
+classdef Drone
+    properties
+        position
+    end
+    methods
+        function obj = Drone(position)
+            obj.position = position
+        end
+    end
+end
