9e4cec3bedd9e8c15dc7c1d708da39c145710339
Antoine Pourchet <antoine.pourchet@gmail.com>
2015-10-29 13:41:25 -0400

    Multiple drones works

--- BasicStrategy.m
+++ BasicStrategy.m
@@ -2,7 +2,10 @@ classdef BasicStrategy < handle
     
     methods
         function drones = initDrones(obj)
-            drones =[Drone([0 0], 1, 1, [1000 1000; 0 0])];
+            drones =[Drone([0 0], 1, 1, [0 1000; 0 0]);
+                Drone([0 0], 1, 1, [1000 1000; 0 0]);
+                Drone([2000 2000], 1, 1, [2500 2000; 2500 2500; 2000 2500; 2000 2000])
+            ];
         end
         function stepDrones(obj, drones, dt)
             for d = 1:length(drones)
